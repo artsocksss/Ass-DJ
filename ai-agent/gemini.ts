4f8db97b380118a1791222b839983563f5d66168
АРТСОК5 <artsocksss@gmail.com>
2026-09-12 15:54:33 +0300

    Add Gemini code
    Signed-off-by: АРТСОК5 <artsocksss@gmail.com>

--- ai-agent/gemini.ts
+++ ai-agent/gemini.ts
@@ -1,1 +1,30 @@
-ai-agent/gemini.ts
+export async function askGemini(prompt: string) {
+  const response = await fetch(
+    "https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-goog-api-key": process.env.GEMINI_API_KEY!,
+      },
+      body: JSON.stringify({
+        contents: [
+          {
+            parts: [
+              {
+                text: prompt,
+              },
+            ],
+          },
+        ],
+      }),
+    }
+  );
+
+  const data = await response.json();
+
+  return (
+    data?.candidates?.[0]?.content?.parts?.[0]?.text ??
+    "Немає відповіді"
+  );
+}
